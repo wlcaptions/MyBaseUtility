159b8f8abf4bfcb6bffdc162dc6d8b5520f01dd8
newmini1 <newmini1@qas-Mac-mini.local>
2024-01-12 14:21:44 +0800

    1

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,35 @@
+//
+//  MyBaseUtilityTests.m
+//  MyBaseUtilityTests
+//
+//  Created by wlcaptions on 01/12/2024.
+//  Copyright (c) 2024 wlcaptions. All rights reserved.
+//
+
+@import XCTest;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
+
